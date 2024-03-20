ccd06d340ca8840358a6d9a7a6320d70de3a2f3d
Igor Bobrukiewicz <igor.bobrukiewicz@gmail.com>
2024-03-20 20:52:42 +0100

    Backend init

---
+++ cyberlarp-api/src/main/java/com/example/cyberlarpapi/CyberlarpApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.cyberlarpapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberlarpApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CyberlarpApiApplication.class, args);
+    }
+
+}
---
+++ cyberlarp-api/src/test/java/com/example/cyberlarpapi/CyberlarpApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cyberlarpapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberlarpApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
