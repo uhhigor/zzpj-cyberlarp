b01bb28b48915332fb7b284590519d6d9807d710
MaciejPerdaszek <maciej.44@wp.pl>
2024-04-05 19:54:11 +0200

    Authentication

---
+++ cyberlarp-api/src/main/java/com/example/cyberlarpapi/Auth0/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.example.cyberlarpapi.Auth0.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.
+                authorizeHttpRequests(authorize -> authorize.requestMatchers("/").permitAll()
+                        .anyRequest().authenticated())
+                .oauth2Login(withDefaults());
+        return http.build();
+    }
+}
