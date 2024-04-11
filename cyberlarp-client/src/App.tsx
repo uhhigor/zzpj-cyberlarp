a420414175c785071cbc20ce1d921c15faeda353
Agnieszka Stasiak <242531@edu.p.lodz.pl>
2024-04-12 01:50:11 +0200

    front started, finished things for auth

--- cyberlarp-client/src/App.tsx
+++ cyberlarp-client/src/App.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Login } from './components/Login';
+import './styles/App.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {Login} from './components/Login';
+import {Main} from "./components/Main";
+import {Dashboard} from "./components/Dashboard";
 
 
 function App() {
-  return (
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={ <Login /> } />
-        </Routes>
-      </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Main/>}/>
+                <Route path='/login' element={<Login/>}/>
+                <Route path='/dashboard' element={<Dashboard/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ cyberlarp-client/src/components/Main.tsx
@@ -0,0 +1,59 @@
+import React, {useState} from "react";
+import "../styles/Main.css"
+import {useAuth0} from "@auth0/auth0-react";
+import {useNavigate} from "react-router-dom";
+
+export const Main = () => {
+    const [isButtonHovered, setIsButtonHovered] = useState(false);
+    const {loginWithRedirect, logout, isAuthenticated} = useAuth0();
+    let navigate = useNavigate();
+
+
+    React.useEffect(() => {
+        if (isAuthenticated) {
+            navigate("/dashboard");
+        }
+    }, [isAuthenticated, navigate]);
+
+    const handleButtonHover = () => {
+        setIsButtonHovered(true);
+    };
+
+    const handleButtonLeave = () => {
+        setIsButtonHovered(false);
+    };
+
+    return (
+        <div className={"Main"}>
+            <div className="d-flex justify-content-center align-items-center min-vh-100">
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col text-center">
+                            <div className={`welcomeText ${isButtonHovered ? 'paused' : ''}`}>CyberLarp
+                            </div>
+                            {!isAuthenticated ? (
+                                    <button
+                                        className="btn btn-lg text-white m-5"
+                                        onMouseEnter={handleButtonHover}
+                                        onMouseLeave={handleButtonLeave}
+                                        onClick={() => loginWithRedirect()}
+                                    >
+                                        Start your story now!
+                                    </button>)
+                                : (
+                                    <div/>
+                                    // <button onClick={() => logout({logoutParams: {returnTo: window.location.origin}})}>
+                                    //     Log Out
+                                    // </button>
+                                )
+                            }
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+
+
+};
