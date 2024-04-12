d1760211176d98b503de05b5e16c7687c2db9111
Tristram02 <wldoekpl@gmail.com>
2024-04-12 22:11:16 +0200

    Added rooms. Adding, removing and making users owners

---
+++ cyberlarp-api/src/main/java/com/example/cyberlarpapi/game/repositories/room/RoomRepository.java
@@ -0,0 +1,7 @@
+package com.example.cyberlarpapi.game.repositories.room;
+
+import com.example.cyberlarpapi.game.data.room.Room;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RoomRepository extends CrudRepository<Room, Integer> {
+}
