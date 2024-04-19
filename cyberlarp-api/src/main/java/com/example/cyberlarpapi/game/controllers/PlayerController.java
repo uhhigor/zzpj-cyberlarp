42b3b3e4dcca3efcbae91d790530263605180d9b
Igor Bobrukiewicz <igor.bobrukiewicz@gmail.com>
2024-04-19 14:09:41 +0200

    PlayerController

--- cyberlarp-api/src/main/java/com/example/cyberlarpapi/game/controllers/PlayerController.java
+++ cyberlarp-api/src/main/java/com/example/cyberlarpapi/game/controllers/PlayerController.java
@@ -1,11 +1,10 @@
 package com.example.cyberlarpapi.game.controllers;
 
+import com.example.cyberlarpapi.game.data.PlayerDTO;
 import com.example.cyberlarpapi.game.exceptions.PlayerServiceException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import com.example.cyberlarpapi.game.services.PlayerService;
 @Controller
 @RequestMapping("/players")
@@ -21,6 +20,30 @@ public class PlayerController {
     public ResponseEntity<Object> getPlayerById(@PathVariable Integer id) {
         try {
             return ResponseEntity.ok(playerService.getById(id));
+        } catch (PlayerServiceException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deletePlayer(@PathVariable Integer id) {
+        playerService.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> createPlayer(@RequestBody PlayerDTO playerDTO) {
+        try {
+            return ResponseEntity.ok(playerService.create(playerDTO));
+        } catch (PlayerServiceException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/{id}")
+    public ResponseEntity<Object> updatePlayer(@PathVariable Integer id, @RequestBody PlayerDTO playerDTO) {
+        try {
+            return ResponseEntity.ok(playerService.update(id, playerDTO));
         } catch (PlayerServiceException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
