3bdff1ab1ecdd779c1e6c44126e12d43623c62cb
Agnieszka Stasiak <242531@edu.p.lodz.pl>
2024-05-08 18:34:42 +0200

    front for chat

--- cyberlarp-client/src/components/Chat.tsx
+++ cyberlarp-client/src/components/Chat.tsx
@@ -1,5 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import axios from "axios";
+import "../styles/Chat.css";
+import {Col, Row} from "react-bootstrap";
+import {useNavigate} from "react-router-dom";
 
 export const Chat = () => {
     const [chatName, setChatName] = useState("");
@@ -19,13 +22,12 @@ export const Chat = () => {
     const handleCreateChat = async (event: { preventDefault: () => void; }) => {
         event.preventDefault();
         try {
-            const response = await axios.post("/api/chat/group", { name: chatName });
+            const response = await axios.post("/api/chat/group", {name: chatName});
             setChatName("");
             setChatId(response.data.id);
             setError(null);
-        } catch (error) {
-            // @ts-ignore
-            setError(error.response.data.message || "Wystąpił błąd podczas tworzenia czatu. Spróbuj ponownie później.");
+        } catch (error: any) {
+            setError(error.response.data.message || "There was an error creating the chat. Please try again later.");
         }
     };
 
@@ -34,18 +36,17 @@ export const Chat = () => {
             const response = await axios.get(`/api/users?search=${searchTerm}`);
             setSearchResults(response.data);
         } catch (error) {
-            console.error("Wystąpił błąd podczas wyszukiwania użytkowników:", error);
+            console.error("There was an error searching for users.", error);
         }
     };
 
     const handleAddUser = async (selectedUserId: any) => {
         try {
-            const response = await axios.post(`/api/chat/group/${chatId}/invite`, { userId: selectedUserId });
+            const response = await axios.post(`/api/chat/group/${chatId}/invite`, {userId: selectedUserId});
             setUserId("");
             setError(null);
-        } catch (error) {
-            // @ts-ignore
-            setError(error.response.data.message || "Wystąpił błąd podczas dodawania użytkownika do czatu. Spróbuj ponownie później.");
+        } catch (error: any) {
+            setError(error.response.data.message || "There was an error adding the user to the chat. Please try again later.");
         }
     };
 
@@ -56,67 +57,77 @@ export const Chat = () => {
     const handleSendMessage = async (event: { preventDefault: () => void; }) => {
         event.preventDefault();
         try {
-            const response = await axios.post(`/api/chat/group/${chatId}/message`, { content: message, senderId: userId });
+            const response = await axios.post(`/api/chat/group/${chatId}/message`, {
+                content: message,
+                senderId: userId
+            });
             setMessage("");
             setError(null);
-        } catch (error) {
-            // @ts-ignore
-            setError(error.response.data.message || "Wystąpił błąd podczas wysyłania wiadomości. Spróbuj ponownie później.");
+        } catch (error: any) {
+            setError(error.response.data.message || "There was an error sending the message. Please try again later.");
         }
     };
 
-    // @ts-ignore
-    // @ts-ignore
-    // @ts-ignore
-    // @ts-ignore
-    // @ts-ignore
+    let navigate = useNavigate();
+    const goBackToDashBoard = () =>{
+        let path = `/dashboard`;
+        navigate(path);
+    }
+
     return (
-        <div>
-            <h2>Chat</h2>
-            <form onSubmit={handleCreateChat}>
-                <input
-                    type="text"
-                    value={chatName}
-                    onChange={(event) => setChatName(event.target.value)}
-                    placeholder="Nazwa czatu"
-                    required
-                />
-                <button type="submit">Utwórz czat</button>
-            </form>
-            <div>
-                <input
-                    type="text"
-                    value={searchTerm}
-                    onChange={(event) => setSearchTerm(event.target.value)}
-                    placeholder="Wyszukaj użytkownika..."
-                />
-                <ul>
-                    {searchResults.length > 0 ? (
-                        <ul>
-                            {searchResults.map((user: { id: number; name: string; }) => (
-                                <li key={user.id}>
-                                    {user.name}{" "}
-                                    <button onClick={() => handleAddUser(user.id)}>Dodaj</button>
-                                </li>
-                            ))}
-                        </ul>
-                    ) : (
-                        <p>Brak wyników wyszukiwania</p>
-                    )}
-                </ul>
-            </div>
-            <form onSubmit={handleSendMessage}>
+        <div className="Chat">
+            <h2 className="p-5">Chats</h2>
+            <Row className="px-5">
+            <Col>
+                <form onSubmit={handleCreateChat}>
+                    <input
+                        type="text"
+                        value={chatName}
+                        onChange={(event) => setChatName(event.target.value)}
+                        placeholder="Chat name"
+                        required
+                    />
+                    <button type="submit" className="btn">Create chat</button>
+                </form>
+                <div>
+                    <input
+                        type="text"
+                        value={searchTerm}
+                        onChange={(event) => setSearchTerm(event.target.value)}
+                        placeholder="Search users..."
+                    />
+                    <ul>
+                        {searchResults.length > 0 ? (
+                            <ul>
+                                {searchResults.map((user: { id: number; name: string; }) => (
+                                    <li key={user.id}>
+                                        {user.name}{" "}
+                                        <button onClick={() => handleAddUser(user.id)}>Add</button>
+                                    </li>
+                                ))}
+                            </ul>
+                        ) : (
+                            <p>No users found</p>
+                        )}
+                    </ul>
+                </div>
+            </Col>
+            <Col>
+                <form onSubmit={handleSendMessage}>
                 <textarea
                     value={message}
                     onChange={handleMessageChange}
-                    placeholder="Wpisz swoją wiadomość..."
+                    placeholder="Write your message here..."
                     rows={4}
                     cols={50}
                     required
                 />
-                <button type="submit">Wyślij</button>
-            </form>
-            {error && <p>{error}</p>}
+                    <button type="submit" className="btn">Send</button>
+                </form>
+                {error && <p>{error}</p>}
+            </Col>
+            </Row>
+            <button className="btn goBack-btn" onClick={goBackToDashBoard}>Go back to dashboard</button>
         </div>
     );
 };
