98c89d92805416e73bf2834b8a27e635727dc2b8
MaciejPerdaszek <maciej.44@wp.pl>
2024-06-14 22:35:37 +0200

    fix

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,7 +3,7 @@ set -xe
 
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://	codedeploystack-webappdeploymentbucket-psbioeeemctu/CyberlarpApiApplication.war /usr/local/tomcat9/webapps/CyberlarpApiApplication.war
+aws s3 cp s3://codedeploystack-webappdeploymentbucket-psbioeeemctu/CyberlarpApiApplication.war /usr/local/tomcat9/webapps/CyberlarpApiApplication.war
 
 
 # Ensure the ownership permissions are correct.
