296dc574f725c80dbb54fb7a625db668a790b549
Igor Bobrukiewicz <igor.bobrukiewicz@gmail.com>
2024-06-18 21:20:55 +0200

    Chat fix

--- cyberlarp-api/src/main/java/com/example/cyberlarpapi/game/controllers/MessageController.java
+++ cyberlarp-api/src/main/java/com/example/cyberlarpapi/game/controllers/MessageController.java
@@ -9,19 +9,18 @@ import com.example.cyberlarpapi.game.exceptions.UserException.UserServiceExcepti
 import com.example.cyberlarpapi.game.model.character.Character;
 import com.example.cyberlarpapi.game.model.chat.SCOPE;
 import com.example.cyberlarpapi.game.model.chat.message.Message;
-import com.example.cyberlarpapi.game.services.CharacterService;
+import com.example.cyberlarpapi.game.model.game.Game;
+import com.example.cyberlarpapi.game.model.user._User;
 import com.example.cyberlarpapi.game.services.GameService;
-import com.example.cyberlarpapi.game.services.MessageService;
 import com.example.cyberlarpapi.game.services.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import javassist.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 @Tag(name = "Message Operations", description = "Operations related to messages in the system")
 @RestController
@@ -33,62 +32,58 @@ public class MessageController {
 
     private final UserService userService;
 
-    private final CharacterService characterService;
-
-    private final MessageService messageService;
-
-    @Operation(summary = "Add message to game", description = "Add message to game by providing game id and content")
-    @PutMapping("/{content}/{scope}/{characterId}")
-    public ResponseEntity<GameController.GameResponse> addMessageToGame(@PathVariable Integer gameId, @PathVariable String content, @PathVariable SCOPE scope, @PathVariable Integer characterId) {
+    @Operation(summary = "Send message in game", description = "Send message in game by providing game id, content and scope")
+    @PostMapping("/")
+    public ResponseEntity<String> addMessageToGame(@PathVariable Integer gameId, @RequestBody MessageRequest messageRequest) {
         try {
-            Character character = characterService.getById(characterId);
-            if(!Objects.equals(character.getUser().getId(), userService.getCurrentUser().getId())) {
-                return ResponseEntity.badRequest().build();
-            }
+            Game game = gameService.getById(gameId);
+            _User user = userService.getCurrentUser();
+            Character character = game.getUserCharacter(user);
+            String content = messageRequest.content;
+            SCOPE scope = messageRequest.scope;
             Message message = new Message(character, scope, content);
             gameService.addMessageToGame(gameId, message);
-            return ResponseEntity.ok(new GameController.GameResponse("Message added to game successfully", gameService.getById(gameId)));
-        } catch (GameNotFoundException | MessageNotFoundException e) {
+            return ResponseEntity.ok("Message " + message.getContent() + " sent in game " + game.getName());
+        } catch (GameNotFoundException | MessageNotFoundException | CharacterNotFoundException e) {
             return ResponseEntity.notFound().build();
-        } catch (CharacterNotFoundException | UserServiceException e) {
-            throw new RuntimeException(e);
+        } catch (UserServiceException e) {
+           return ResponseEntity.badRequest().build();
         }
     }
 
-    @Operation(summary = "Delete message from game", description = "Delete message from game by providing game id and message id")
-    @DeleteMapping("/{messageId}")
-    public ResponseEntity<GameController.GameResponse> deleteMessageFromGame(@PathVariable Integer gameId, @PathVariable Integer messageId) {
+    @Operation(summary = "Get messages from game", description = "Get messages from game by providing game id and scope")
+    @GetMapping("/{scope}")
+    public ResponseEntity<List<String>> getMessagesFromGame(@PathVariable Integer gameId, @PathVariable SCOPE scope) {
         try {
-            Message message = messageService.getMessageById(messageId);
-            gameService.deleteMessageFromGame(gameId, message);
-            return ResponseEntity.ok(new GameController.GameResponse("Message deleted from game successfully", gameService.getById(gameId)));
-        } catch (GameNotFoundException | MessageNotFoundException | NotFoundException e) {
+            Game game = gameService.getById(gameId);
+            _User user = userService.getCurrentUser();
+            Character character = game.getUserCharacter(user);
+            List<Message> messages = gameService.getMessagesFromGame(gameId, character, scope);
+            List<String> messageContents = new ArrayList<>();
+            for (Message message : messages) {
+                messageContents.add(message.getTimestamp() + " [" + message.getScope() + "] " + message.getSender().getName() + ": " + message.getContent());
+            }
+            return ResponseEntity.ok(messageContents);
+        } catch (CharacterNotFoundException | GameNotFoundException e) {
             return ResponseEntity.notFound().build();
+        } catch (InvalidFactionException | UserServiceException e) {
+            return ResponseEntity.badRequest().build();
         }
     }
 
-    @Operation(summary = "Get messages by game id, character id and scope", description = "Get messages by game id, character id and scope")
-    @GetMapping("/{characterId}/{scope}")
-    public ResponseEntity<List<Message>> getMessagesFromGame(@PathVariable Integer gameId, @PathVariable Integer characterId, @PathVariable SCOPE scope) {
-        try {
-            Character character = characterService.getById(characterId);
-            return ResponseEntity.ok(gameService.getMessagesFromGame(gameId, character, scope));
-        } catch (CharacterNotFoundException e) {
-            return ResponseEntity.notFound().build();
-        } catch (InvalidFactionException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    class MessageResponse {
+    public static class MessageResponse {
         private final String message;
 
         public MessageResponse(String message) {
             this.message = message;
         }
 
-        public String getMessage() {
-            return message;
-        }
+    }
+
+
+    public static class MessageRequest {
+        public String content;
+        public SCOPE scope;
+
     }
 }
