3460f5522b0a6ea372e871a29a85c4f9158ca0c1
Agnieszka Stasiak <242531@edu.p.lodz.pl>
2024-06-18 14:29:17 +0200

    fix

--- cyberlarp-api/src/test/java/com/example/cyberlarpapi/unit/DefaultGameDataTests.java
+++ cyberlarp-api/src/test/java/com/example/cyberlarpapi/unit/DefaultGameDataTests.java
@@ -2,7 +2,6 @@ package com.example.cyberlarpapi.unit;
 
 import com.example.cyberlarpapi.game.DefaultGameData;
 import com.example.cyberlarpapi.game.model.character.Character;
-import com.example.cyberlarpapi.game.model.character.faction.Faction;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -10,7 +9,6 @@ import java.util.List;
 public class DefaultGameDataTests {
     DefaultGameData defaultGameData = new DefaultGameData();
     List<Character> defaultCharacters = defaultGameData.getDefaultCharacters();
-    List<Faction> defaultFactions = defaultGameData.getDefaultFactions();
     @Test
     public void testDefaultCharactersData() {
         assert defaultCharacters.size() == 3;
@@ -50,20 +48,5 @@ public class DefaultGameDataTests {
         assert defaultCharacters.get(2).getKnowledge() == 6;
         assert defaultCharacters.get(2).getMaxHp() == 8;
         assert defaultCharacters.get(2).getArmor() == 1;
-    }
-
-
-    @Test
-        public void testDefaultFactionsData() {
-
-        assert defaultFactions.size() == 3;
-        assert defaultFactions.get(0).getName().equals("Arasaka");
-        assert defaultFactions.get(0).getDescription().equals("Arasaka Corporation is a world-wide megacorporation dealing in corporate security, banking, and manufacturing.");
-
-        assert defaultFactions.get(1).getName().equals("Militech");
-        assert defaultFactions.get(1).getDescription().equals("Militech International Armaments is a megacorporation specializing in weapons manufacturing and private military contracting.");
-
-        assert defaultFactions.get(2).getName().equals("Trauma Team International");
-        assert defaultFactions.get(2).getDescription().equals("Trauma Team International is a megacorporation specializing in medical services and paramilitary operations.");
     }
 }
